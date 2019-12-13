06ba2f3db8b88bcab98df521645627a5b7e081a8
Drsongl <1044241610@qq.com>
2019-12-13 21:18:53 +0800

    change work dir

---
+++ src_new/com/bean/AdviceBean.java
@@ -0,0 +1,61 @@
+package com.bean;
+public class AdviceBean {
+
+    private int Ono ;
+    private int Cno;
+	private int Pno ;
+	private String Oname ;
+	private Date Ontime ;
+	private Date Offtime ;
+	private int Ostatus ;
+	private int Sno ;
+	
+	public int getOno() {
+		return Ono;
+	}
+	public void setOno(int ono) {
+		Ono = ono;
+	}
+	public int getCno() {
+		return Ono;
+	}
+	public void setCno(int cno) {
+		Cno = cno;
+	}
+	public int getPno() {
+		return Pno;
+	}
+	public void setPno(int pno) {
+		Pno = pno;
+	}
+	public String getOname() {
+		return Oname;
+	}
+	public void setOname(String oname) {
+		Oname = oname;
+	}
+	public Date getOntime() {
+		return Ontime;
+	}
+	public void setOntime(Date ontime) {
+		Ontime = ontime;
+	}	
+	public Date getOfftime() {
+		return Offtime;
+	}
+	public void setOfftime(Date offtime) {
+		Offtime = offtime;
+	}
+	public int getOstatus() {
+		return Onstatus;
+	}
+	public void setOnstatus(int ostatus) {
+		Ostatus = ostatus;
+	}
+	public int getSno() {
+		return Sno;
+	}
+	public void setSno(int sno) {
+		Sno = sno;
+	}
+}
---
+++ src_new/com/bean/SubBean.java
@@ -0,0 +1,26 @@
+package com.bean;
+public class SubBean {
+
+    private int Subno ;
+    private String Subname ;
+    private String Subloc ;
+	
+	public String getSubno() {
+		return Subno;
+	}
+	public void setSubno(String subno) {
+		Subno = subno;
+	}
+	public String getSubname() {
+		return Subname;
+	}
+	public void setSubname(String subname) {
+		Subname = subname;
+	}
+	public String getSubloc() {
+		return Subloc;
+	}
+	public void setSubloc(String subloc) {
+		Subloc = subloc;
+	}	
+}
