2f89bd86af4c0fc5ebb1ec3488c686d95926f492
Drsongl <1044241610@qq.com>
2019-12-14 15:26:08 +0800

    修改成立单个超市的表，后续还需要对id类的属性建立自增属性

--- supermarket.sql
+++ supermarket.sql
@@ -1,51 +1,35 @@
 CREATE DATABASE supermarket;
 USE supermarket;
 
-/* public information*/
-CREATE SCHEMA general;asd
-asd
-
-CREATE SCHEMA general;
-CREATE SCHEMA general;
- 
-aaa
-
-dfg
 -- 以下定义的int长度可以表示最大的数为10位，数据字典为12位，但bigint可以表示的数又过于大了
 
 CREATE TABLE user_info(
     -- 用户信息表，包括用户名，密码，用户类型
-    Username int PRIMARY KEY,--数据字典中没有定义
-    "Password" char(12) not NULL, --数据字典没有定义
-    User_type char(2) not NULL --数据字典中没有定义
+    Username int PRIMARY KEY,--数据字典中没有定义       用户名
+    "Password" char(12) not NULL, --数据字典没有定义    密码
+    User_type char(2) not NULL --数据字典中没有定义     用户类型
 );
 
 CREATE TABLE supplier(
     -- 供应商信息表
     -- supplier information 'provider'
     -- since we have all-storage, we should have a Prono & Proname for all-storage
-    Prono int PRIMARY KEY,--数据字典中没有定义
-    Proname char(12) not NULL, --数据字典没有定义
-);
-
-CREATE TABLE sub(
-    -- 超市信息表/仓库信息表
-    Subno int PRIMARY KEY, -- 超市编号/仓库编号,数据字典中无
-    Subname char(12) unique, -- 超市名称/仓库名称，数据字典中无
-    Subloc char(20) not NULL, -- 超市位置/仓库位置，数据字典中无
+    Prono int PRIMARY KEY,--数据字典中没有定义           供应商编号
+    Proname char(12) not NULL, --数据字典没有定义        供应商名称
 );
 
 CREATE TABLE product_info(
     -- 商品信息表
     -- I add Vno, Vname, Lno, Lname
-    Pno int PRIMARY KEY,
-    Pname char(30) not NULL unique,
-    Sellprice NUMERIC(4,2) not NULL,
-    Vno int not NULL,
-    Lno int not NULL,
-    Shelfno int not NULL, -- selfno  可以累积，比如一个超市是1，2，3，另一个超市是4，5，6，这样，就没有问题了
+    Pno int PRIMARY KEY,   --                           商品编号
+    Pname char(30) not NULL unique,  --                 商品名称
+    Sellprice NUMERIC(4,2) not NULL,  --                售价
+    Vno int not NULL,  --                               大类编号
+    Lno int not NULL,  --                               小类编号
+    Shelfno int not NULL, --                            货架编号 
+    -- selfno  可以累积，比如一个超市是1，2，3，另一个超市是4，5，6，这样，就没有问题了
     -- we may put shelfno here, not in replenish table, but the problem is shelfno for each supermarket is different.
-    --Stockout_n int not NULL 表示商品缺货报警线
+    Stockout_n int not NULL, -- 表示商品缺货报警线 -- 安澜设计的是0、1，我想的是该商品报警线，有待进一步交流
 );
 
 /*
@@ -53,61 +37,50 @@ CREATE TABLE product_info(
 */
 CREATE TABLE producttree(
     -- 商品分类树 
-    Vno int not NULL,
-    Vname char(20) not NULL,
-    Lno int not NULL,
-    Lname char(20) not NULL,
+    Vno int not NULL,                                               -- 商品大类编号
+    Vname char(20) not NULL,                                        -- 商品大类名称
+    Lno int not NULL,                                               -- 商品小类编号
+    Lname char(20) not NULL,                                        -- 商品小类名称
     PRIMARY KEY (Vno, Lno),
 );
 
-CREATE TABLE all_replenish(
-    -- 总仓库供应表
+CREATE TABLE replenish(
+    -- 供应信息表
     -- I delete selfno, add Inum(input product number), 
-    Ino int PRIMARY KEY,
-    Prono int foreign key references supplier(Prono),
-    Pno int foreign key references product_info(Pno),
-    Inum int not NULL,-- 新增的属性，数据字典中无
-    Grossprice NUMERIC(4,2) not NULL,-- 数据字典中定义为double，此处考虑实际修改为numeric
-    Sno int foreign key references all_worker_info(Sno),
-    Idate date not NULL,-- Shelfdate the name is misleading, maybe Idate will be better
+    Ino int PRIMARY KEY,                                                                -- 进货编号
+    Prono int foreign key references supplier(Prono),                                   -- 供应商编号
+    Pno int foreign key references product_info(Pno),                                   -- 商品编号
+    Inum int not NULL,-- 新增的属性，数据字典中无                                           -- 一次进货的数量
+    Grossprice NUMERIC(4,2) not NULL,-- 数据字典中定义为double，此处考虑实际修改为numeric     -- 进货价
+    Sno int foreign key references all_worker_info(Sno),                                -- 员工编号 
+    Idate date not NULL,-- Shelfdate the name is misleading, maybe Idate will be better -- 进货日期
 );
 
-CREATE TABLE all_replace(
-    -- 总仓库调出表
-    -- 仓库调货出去，包括商品编号，数量，调货目的地（超市编号）
-    Pno int PRIMARY KEY,
-    Re_Quantity int not NULL,
-    Subno int foreign key REFERENCES sub(Subno),
-);
 
-CREATE TABLE all_storage(
+CREATE TABLE storage(
     -- 总仓库库存表
-    Pno int PRIMARY KEY,
-    Pname char(30) not NULL unique,
-    All_Quantity int not NULL,
-    -- 安澜设计的是0、1，我想的是该商品报警线，有待进一步交流
+    Pno int PRIMARY KEY, -- 商品编号
+    Pname char(30) not NULL unique, -- 商品名称
+    Quantity int not NULL, -- 商品库存数量
+    
 );
 
 CREATE TABLE customer_info (
     -- 消费者信息表
-    Cno int PRIMARY KEY,
-    Cgrade smallint not NULL,
-    Cname char(16) not NULL,
-    Cgender smallint not NULL,
+    Cno int PRIMARY KEY, -- 消费者编号
+    Cgrade smallint not NULL, -- 消费者的年级
+    Cname char(16) not NULL, -- 消费者姓名
+    Cgender smallint not NULL, -- 消费者性别
 );
 
-CREATE TABLE all_worker_info(
+CREATE TABLE worker_info(
     -- 工作人员信息表
-    Sno int PRIMARY KEY,
-    Sjob char(4) not NULL, -- 数据字典中无
-    Sgrade smallint not NULL,
-    Sname char(16) not NULL,
+    Sno int PRIMARY KEY, -- 员工编号
+    Sjob char(4) not NULL, -- 数据字典中无 -- 员工职位
+    Sgrade smallint not NULL, -- 员工年级
+    Sname char(16) not NULL, -- 员工姓名
 );
 
-
-/* private infomation for each supermarket */
-CREATE SCHEMA super1;
-USE super1;
 
 CREATE TABLE traffic_info (
     -- 交易数据 transaction data
@@ -117,26 +90,6 @@ CREATE TABLE traffic_info (
     Pno int unique not NULL foreign key references general.product_info(Pno),
     Cno int unique not NULL foreign key references customer_info(Cno),
     Sno int unique not NULL foreign key references worker_info(sno),
-);
-
-CREATE TABLE replenish(
-    -- 该超市的进货信息表
-    -- I delete selfdate, selfno, add Inum(input product number)
-    Ino int PRIMARY KEY,
-    Prono int foreign key references general.supplier(Prono),
-    Pno int foreign key references general.product_info(Pno),
-    Inum int not NULL,-- 新增的属性，数据字典中无
-    Grossprice NUMERIC(4,2) not NULL,-- 数据字典中定义为double，此处考虑实际修改为numeric
-    Sno int foreign key references worker_info(Sno),
-    Idate date not NULL,-- the name is misleading, maybe Idate will be better
-);
-
-CREATE TABLE storage(
-    -- 该超市的库存信息表
-    Pno int PRIMARY KEY,
-    Pname char(30) not NULL unique,
-    Quantity int not NULL,
-    -- Stockout_Flag int not NULL,-- 安澜设计的是0、1，我想的是该商品报警线，有待进一步交流
 );
 
 CREATE TABLE require_info (
@@ -149,14 +102,6 @@ CREATE TABLE require_info (
     Offtime date not NULL,
     Ostatus smallint not NULL,
     Sno int foreign key references worker_info(Sno),
-);
-
-CREATE TABLE worker_info(
-    -- 工作人员信息表
-    Sno int PRIMARY KEY,
-    Sjob char(4) not NULL, -- 数据字典中无
-    Sgrade smallint not NULL,
-    Sname char(16) not NULL,
 );
 
 CREATE TABLE shelf(
