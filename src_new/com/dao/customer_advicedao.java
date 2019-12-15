ac628c5723459ebfa411bd5215bc73880ddbc90d
khj1212k <58841556+khj1212k@users.noreply.github.com>
2019-12-15 17:07:32 +0800

    customer2

--- src_new/com/dao/customer_advicedao.java
+++ src_new/com/dao/customer_advicedao.java
@@ -7,29 +7,26 @@ import java.sql.*;
 public class customer_AdviceDao {
 
     //모든건의,건의검색결과,이전건의,건의하기
-    //모든건의获取所有列表
-    public AdviceBean GetList(final Int ostatus){
-		String sql="select Ono, Oname, Cno, Pno, 
-        Ontime, Offtime, Ostatus, Sno from  Advice  
-        where Ostatus="+ostatus;
+    //모든건의获取所有列表v
+    public AdviceBean GetAllList(final Int Cno){
+		final String sql="select Ono, Oname, Cno, Ontime, Offtime, Ostatus, Sno from  Advice ";
 		Statement stat = null;
 		ResultSet rs = null;
-		Connection conn = new DBHelper().getConn();
-		List<AdviceBean> list=new ArrayList<AdviceBean>();
+		final Connection conn = new DBHelper().getConn();
+		final List<AdviceBean> list=new ArrayList<AdviceBean>();
 		try{
 			stat = conn.createStatement();
 			rs = stat.executeQuery(sql);
 			while(rs.next()){
 				cnbean.setOno(rs.getInt("ono"));
-				cnbean.setOname(rs.getstring("oname"));
+				cnbean.setOname(rs.getString("oname"));
 				cnbean.setCno(rs.getInt("cno"));
-				cnbean.setPno(rs.getInt("pno"));
 				cnbean.setOntime(rs.getDate("ontime"));
 				cnbean.setOfftime(rs.getDate("offtime"));
 				cnbean.setOstatus(rs.int("ostatus"));
 				list.add(cnbean);
 			}
-		} catch (SQLException e) {
+		} catch (final SQLException e) {
 			e.printStackTrace();
 		} finally {
 			try {
@@ -39,52 +36,122 @@ public class customer_AdviceDao {
 					stat.close();
 				if (rs != null)
 					rs.close();
-			} catch (SQLException e) {
+			} catch (final SQLException e) {
 				e.printStackTrace();
 			}
 		}
 		return list;
     }
-    
-    //건의검색결과 建议查询
-    public AdviceBean GetList(final Int ostatus){
-		String sql="select Ono, Oname, Cno, Pno, 
-        Ontime, Offtime, Ostatus, Sno from  Advice  
-        where Ostatus="+ostatus;
-		Statement stat = null;
-		ResultSet rs = null;
-		Connection conn = new DBHelper().getConn();
-		List<AdviceBean> list=new ArrayList<AdviceBean>();
-		try{
-			stat = conn.createStatement();
-			rs = stat.executeQuery(sql);
-			while(rs.next()){
-				cnbean.setOno(rs.getInt("ono"));
-				cnbean.setOname(rs.getstring("oname"));
-				cnbean.setCno(rs.getInt("cno"));
-				cnbean.setPno(rs.getInt("pno"));
-				cnbean.setOntime(rs.getDate("ontime"));
-				cnbean.setOfftime(rs.getDate("offtime"));
-				cnbean.setOstatus(rs.int("ostatus"));
-				list.add(cnbean);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (conn != null)
-					conn.close();
-				if (stat != null)
-					stat.close();
-				if (rs != null)
-					rs.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return list;
-    }
-    //이전건의 Cno=顾客编号
 
-    //건의하기
+	// 건의검색결과 建议查询(用户名)v
+	public AdviceBean GetList(final Int Cno){
+		final String sql="select Ono, Oname, Cno, Ontime, Offtime, Ostatus, Sno from  Advice where Cno="+Cno;
+		Statement stat = null;
+		ResultSet rs = null;
+		final Connection conn = new DBHelper().getConn();
+		final List<AdviceBean> list=new ArrayList<AdviceBean>();
+		try{
+			stat = conn.createStatement();
+			rs = stat.executeQuery(sql);
+			while(rs.next()){
+				cnbean.setOno(rs.getInt("ono"));
+				cnbean.setOname(rs.getString("oname"));
+				cnbean.setCno(rs.getInt("cno"));
+				cnbean.setPno(rs.getInt("pno"));
+				cnbean.setOntime(rs.getDate("ontime"));
+				cnbean.setOfftime(rs.getDate("offtime"));
+				cnbean.setOstatus(rs.int("ostatus"));
+				list.add(cnbean);
+			}
+		} catch (final SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+				if (stat != null)
+					stat.close();
+				if (rs != null)
+					rs.close();
+			} catch (final SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+
+	// 건의검색결과 建议查询(time)v
+	public AdviceBean GetList(final Int Ontime){
+			final String sql="select Ono, Oname, Cno, Ontime, Offtime, Ostatus, Sno from  Advice where Ontime="+Ontime;
+			Statement stat = null;
+			ResultSet rs = null;
+			final Connection conn = new DBHelper().getConn();
+			final List<AdviceBean> list=new ArrayList<AdviceBean>();
+			try{
+				stat = conn.createStatement();
+				rs = stat.executeQuery(sql);
+				while(rs.next()){
+					cnbean.setOno(rs.getInt("ono"));
+					cnbean.setOname(rs.getString("oname"));
+					cnbean.setCno(rs.getInt("cno"));
+					cnbean.setPno(rs.getInt("pno"));
+					cnbean.setOntime(rs.getDate("ontime"));
+					cnbean.setOfftime(rs.getDate("offtime"));
+					cnbean.setOstatus(rs.int("ostatus"));
+					list.add(cnbean);
+				}
+			} catch (final SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (conn != null)
+						conn.close();
+					if (stat != null)
+						stat.close();
+					if (rs != null)
+						rs.close();
+				} catch (final SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			return list;
+		}
+	// 이전건의 以前的建议 Cno=自己的顾客编号 x
+
+	public AdviceBean GetList(final Int Cno){
+		final String sql="select Ono, Oname, Cno, Ontime, Offtime, Ostatus, Sno from  Advice where Cno="+Cno;
+		Statement stat = null;
+		ResultSet rs = null;
+		final Connection conn = new DBHelper().getConn();
+		final List<AdviceBean> list=new ArrayList<AdviceBean>();
+		try{
+			stat = conn.createStatement();
+			rs = stat.executeQuery(sql);
+			while(rs.next()){
+				cnbean.setOno(rs.getInt("ono"));
+				cnbean.setOname(rs.getString("oname"));
+				cnbean.setCno(rs.getInt("cno"));
+				cnbean.setPno(rs.getInt("pno"));
+				cnbean.setOntime(rs.getDate("ontime"));
+				cnbean.setOfftime(rs.getDate("offtime"));
+				cnbean.setOstatus(rs.int("ostatus"));
+				list.add(cnbean);
+			}
+		} catch (final SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+				if (stat != null)
+					stat.close();
+				if (rs != null)
+					rs.close();
+			} catch (final SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+    //건의하기 zuojianyi
 }
--- src_new/com/dao/customer_productdao.java
+++ src_new/com/dao/customer_productdao.java
@@ -8,7 +8,9 @@ import java.sql.*;
 public class customer_ProductDao {
 
     //获取所有列表
-    public ProductBean GetAllList(final String Pname){
+  
+
+	public ProductBean GetAllList(final String Pno){
 		final String sql=
 		"select Pno,Pname,Sellprice,Shelfno,Shelfloc from product_info,shelf where product_info.Shelfno=shelf.Shelfno ";
 		Statement stat = null;
@@ -19,8 +21,9 @@ public class customer_ProductDao {
 			stat = conn.createStatement();
 			rs = stat.executeQuery(sql);
 			while(rs.next()){
+				ProductBean cnbean=new ProductBean();
 				cnbean.setPno(rs.getInt("pno"));
-				cnbean.setPname(rs.getstring("pname"));
+				cnbean.setPname(rs.getString("pname"));o
 				cnbean.setSellprice(rs.getString("sellprice"));
 				cnbean.setShelfno(rs.getInt("shelfno"));
 				cnbean.setShelfloc(rs.getString("shelfloc"));
@@ -41,23 +44,27 @@ public class customer_ProductDao {
 				e.printStackTrace();
 			}
 		}
-		return cnbean;
-    }    
-    
-    //商品信息 （商品名称 查询）
-    public ProductBean GetList(final String Pname){
-		final String sql=
-		"select Pno, Pname,Sellprice, Shelfno, Shelfloc from product_info,shelf where product_info.Shelfno=shelf.Shelfno and Pname="+Pname;
+		return list;
+    }
+
+	private void setSellprice(String string) {
+	}
+
+	// 商品信息 （商品名称 查询）
+	public ProductBean GetList(final String Pname) {
+		final String sql = "select Pno, Pname,Sellprice, Shelfno, Shelfloc from product_info,shelf where product_info.Shelfno=shelf.Shelfno and Pname="
+				+ Pname;
 		Statement stat = null;
 		ResultSet rs = null;
 		final Connection conn = new DBHelper().getConn();
-		final List<ProductBean> list=new ArrayList<ProductBean>();
-		try{
+		final List<ProductBean> list = new ArrayList<ProductBean>();
+		try {
 			stat = conn.createStatement();
 			rs = stat.executeQuery(sql);
-			while(rs.next()){
+			while (rs.next()) {
+				ProductBean cnbean=new ProductBean()
 				cnbean.setPno(rs.getInt("pno"));
-				cnbean.setPname(rs.getstring("pname"));
+				cnbean.setPname(rs.getString("pname"));
 				cnbean.setSellprice(rs.getString("sellprice"));
 				cnbean.setShelfno(rs.getInt("shelfno"));
 				cnbean.setShelfloc(rs.getString("shelfloc"));
@@ -78,7 +85,45 @@ public class customer_ProductDao {
 				e.printStackTrace();
 			}
 		}
-		return cnbean;
+		return list;
+	}
+	
+	// 商品信息 （商品pno查询）
+	public ProductBean GetList(final String Pname) {
+		final String sql = "select Pno, Pname,Sellprice, Shelfno, Shelfloc from product_info,shelf where product_info.Shelfno=shelf.Shelfno and Pno="
+				+ Pno;
+		Statement stat = null;
+		ResultSet rs = null;
+		final Connection conn = new DBHelper().getConn();
+		final List<ProductBean> list = new ArrayList<ProductBean>();
+		try {
+			stat = conn.createStatement();
+			rs = stat.executeQuery(sql);
+			while (rs.next()) {
+				ProductBean cnbean=new ProductBean()
+				cnbean. setPno(rs.getInt("pno"));
+				cnbean.setPname(rs.getString("pname"));
+				cnbean.setSellprice(rs.getString("sellprice"));
+				cnbean.setShelfno(rs.getInt("shelfno"));
+				cnbean.setShelfloc(rs.getString("shelfloc"));
+				
+				list.add(cnbean);
+			}
+		} catch (final SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (conn != null)
+					conn.close();
+				if (stat != null)
+					stat.close();
+				if (rs != null)
+					rs.close();
+			} catch (final SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
     }    
 }
 
