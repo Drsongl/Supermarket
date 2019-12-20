8985ed994ae605d783370f122e4d08fd21a32c1c
zhuziyi1997 <1305202295@qq.com>
2019-12-20 20:29:58 +0800

    zzy 尚未完成先保存一部分的sql

---
+++ database-zzy.sql
@@ -0,0 +1,167 @@
+CREATE DATABASE supermarket;
+USE supermarket;
+
+-- 以下定义的int长度可以表示最大的数为10位，数据字典为12位，但bigint可以表示的数又过于大了
+
+CREATE TABLE user_info(
+    -- 用户信息表，包括用户名，密码，用户类型
+    Uno int identity(1,1) PRIMARY KEY,
+    Username varchar(12) not NULL,--数据字典中没有定义   用户名
+    "Password" char(12) not NULL, --数据字典没有定义    密码
+    Cno int UNIQUE,
+    "Type" char(2) not NULL --数据字典中没有定义     用户类型
+);
+
+CREATE TABLE supplier(
+    -- 供应商信息表
+    -- supplier information 'provider'
+    -- since we have all-storage, we should have a Prono & Proname for all-storage
+    Prono int identity(1,1) PRIMARY KEY,--数据字典中没有定义           供应商编号
+    Proname char(12) not NULL, --数据字典没有定义        供应商名称
+);
+
+CREATE TABLE product_info(
+    -- 商品信息表
+    -- I add Vno, Vname, Lno, Lname
+    Pno int identity(1,1) PRIMARY KEY,   --                           商品编号
+    Pname char(30) not NULL unique,  --                 商品名称
+    Sellprice NUMERIC(4,2) not NULL,  --                售价
+    Vno int not NULL,  --                               大类编号
+    Lno int not NULL,  --                               小类编号
+    Shelfno int not NULL, --                            货架编号 
+    -- selfno  可以累积，比如一个超市是1，2，3，另一个超市是4，5，6，这样，就没有问题了
+    -- we may put shelfno here, not in replenish table, but the problem is shelfno for each supermarket is different.
+    Stockout_n int not NULL, -- 表示商品缺货报警线 -- 安澜设计的是0、1，我想的是该商品报警线，有待进一步交流
+);
+
+/*
+新建了一个表，主要是商品分类树是一个相对独立的实体
+*/
+CREATE TABLE producttree(
+    -- 商品分类树 
+    Vno int identity(1,1) not NULL,                                               -- 商品大类编号
+    Vname char(20) not NULL,                                        -- 商品大类名称
+    Lno int identity(1,1) not NULL,                                               -- 商品小类编号
+    Lname char(20) not NULL,                                        -- 商品小类名称
+    PRIMARY KEY (Vno,Lno)
+);
+
+CREATE TABLE replenish(
+    -- 供应信息表
+    -- I delete selfno, add Inum(input product number), 
+    Ino int identity(1,1) PRIMARY KEY,                                                                -- 进货编号
+    Prono int foreign key references supplier(Prono),                                   -- 供应商编号
+    Pno int foreign key references product_info(Pno),                                   -- 商品编号
+    Inum int not NULL,-- 新增的属性，数据字典中无                                           -- 一次进货的数量
+    Grossprice NUMERIC(4,2) not NULL,-- 数据字典中定义为double，此处考虑实际修改为numeric     -- 进货价
+    Sno int foreign key references worker_info(Sno),                                -- 员工编号 
+    Idate date not NULL,-- Shelfdate the name is misleading, maybe Idate will be better -- 进货日期
+);
+
+
+CREATE TABLE storage(
+    -- 总仓库库存表
+    Pno int PRIMARY KEY, -- 商品编号
+    Pname char(30) not NULL unique, -- 商品名称
+    Quantity int not NULL, -- 商品库存数量
+    
+);
+
+CREATE TABLE customer_info (
+    -- 消费者信息表
+    Cno int PRIMARY KEY, -- 消费者编号，学号
+    Cgrade smallint not NULL, -- 消费者的年级
+    Cname char(16) not NULL, -- 消费者姓名
+    Cgender smallint not NULL, -- 消费者性别
+);
+
+CREATE TABLE worker_info(
+    -- 工作人员信息表
+    Sno int PRIMARY KEY, -- 员工编号，学号
+    Sjob char(4) not NULL, -- 数据字典中无 -- 员工职位
+    Sgrade smallint not NULL, -- 员工年级
+    Sname char(16) not NULL, -- 员工姓名
+);
+
+
+CREATE TABLE traffic_info (
+    -- 交易数据 transaction data
+    Tno int identity(1,1) PRIMARY KEY,
+    Ttime date not NULL,
+    Gno int not NULL,
+    Pno int unique not NULL foreign key references product_info(Pno),
+    Cno int unique not NULL foreign key references customer_info(Cno),
+    Sno int unique not NULL foreign key references worker_info(sno),
+);
+
+CREATE TABLE require_info (
+    -- 消费者建议
+    Ono int identity(1,1) PRIMARY KEY,
+    Cno int not NULL foreign key references customer_info(Cno),
+    Oname char(50) not NULL,
+    Ontime date not NULL,
+    Offtime date not NULL,
+    Ostatus smallint not NULL,
+    Sno int foreign key references worker_info(Sno),
+);
+
+CREATE TABLE shelf(
+    -- 货架信息表
+    Shelfno int identity(1,1) PRIMARY KEY,
+    Vno int not NULL,
+    Shelfloc char(10), --数据字典中无
+    Sno int foreign key references worker_info(Sno),
+);
+
+
+---插入值
+-----supplier
+insert 
+into supplier(Proname)
+values ('旺仔')
+
+insert 
+into supplier(Proname)
+values ('统一')
+
+----product
+insert 
+into product_info(Pname,Sellprice,Vno,Lno,Shelfno,Stockout_n)
+values ('QQ糖',4,1,7,11,300)
+
+insert 
+into product_info(Pname,Sellprice,Vno,Lno,Shelfno,Stockout_n)
+values ('冰红茶',3.5,2,2,5,80)
+
+----storage
+insert 
+into storage(Pname,Pno,Quantity)
+values ('QQ糖',1,330)
+
+----customer
+insert 
+into customer_info(Cno,Cname,Cgrade,Cgender)
+values (163071,'束巨酷',4,1)
+
+insert 
+into customer_info(Cno,Cname,Cgrade,Cgender)
+values (173071,'辛希办',3,2)
+
+----worker
+insert 
+into worker_info(Sno,Sname,Sgrade,Sjob)
+values (163072,'谢程',4,'员工')
+
+insert 
+into worker_info(Sno,Sname,Sgrade,Sjob)
+values (163073,'焦武初',4,'经理')
+
+----shelf
+INSERT
+INTO shelf(Vno,Shelfloc,Sno)
+VALUES (1,11,163072)
+
+----replenish
+INSERT
+INTO replenish(Prono,Pno,Inum,Grossprice,Sno,Idate)
+VALUES (1,1,200,2.2,163072,'2019-12-20')
